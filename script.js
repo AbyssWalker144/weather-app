23c057ac10ae80aedaaac630190c8836eff9938f
Valeriy <balorok5@gmail.com>
2023-01-02 21:12:15 +0200

    display some data

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ console.log('hello');
 const API_KEY = '6e01dacfa1ae7c17d7d7ca01644c735a';
 
 const temperature = $('.temp');
+const mainImg = $('.today__img');
+const mainCondition = $('.condition');
+const wind = $('.wind').children('p');
+const pressure = $('.pressure').children('p');
+const humidity = $('.humidity').children('p');
 
 (function getWeatherData () {
     console.log('hi');
@@ -13,11 +18,17 @@ const temperature = $('.temp');
         console.log(data);
     });
 
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${API_KEY}&units=metric`)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=50.45&lon=30.52&appid=${API_KEY}&units=metric`)
     .then(res2 => res2.json())
     .then(data2 => {
         console.log(data2);
         temperature.text(Math.round(data2.list[0].main.temp));
+        
+        mainImg.attr('src', `http://openweathermap.org/img/wn/${data2.list[0].weather[0].icon}@4x.png`)
+        mainCondition.text(data2.list[0].weather[0].main.toUpperCase());
+        wind.text(`${data2.list[0].wind.speed} m/s`);
+        pressure.text(`${data2.list[0].main.pressure} hPa`);
+        humidity.text(`${data2.list[0].main.humidity} %`)
     });
 })();
 
