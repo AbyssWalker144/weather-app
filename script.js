02875f2fe288d73086860e8596575de5c5269dca
AbyssWalker144 <balorok5@gmail.com>
2023-01-02 14:29:14 +0200

    fetch test 2

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log('hello');
 
 const API_KEY = '6e01dacfa1ae7c17d7d7ca01644c735a';
 
+const temperature = $('.temp');
 
 (function getWeatherData () {
     console.log('hi');
@@ -12,9 +13,13 @@ const API_KEY = '6e01dacfa1ae7c17d7d7ca01644c735a';
         console.log(data);
     });
 
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${API_KEY}`)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${API_KEY}&units=metric`)
     .then(res2 => res2.json())
     .then(data2 => {
         console.log(data2);
+        temperature.text(Math.round(data2.list[0].main.temp));
     });
 })();
+
+
+console.log(temperature)
