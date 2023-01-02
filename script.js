d90eb339a16362eb7fec3b1621eaa816a10da58e
Valeriy <balorok5@gmail.com>
2023-01-02 21:18:27 +0200

    display some data

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log('hello');
 
 const API_KEY = '6e01dacfa1ae7c17d7d7ca01644c735a';
 
@@ -12,19 +11,19 @@ const humidity = $('.humidity').children('p');
 (function getWeatherData () {
     console.log('hi');
 
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${API_KEY}`)
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${API_KEY}`)
     .then(res => res.json())
     .then(data => {
         console.log(data);
     });
 
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=50.45&lon=30.52&appid=${API_KEY}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=50.45&lon=30.52&appid=${API_KEY}&units=metric`)
     .then(res2 => res2.json())
     .then(data2 => {
         console.log(data2);
         temperature.text(Math.round(data2.list[0].main.temp));
         
-        mainImg.attr('src', `http://openweathermap.org/img/wn/${data2.list[0].weather[0].icon}@4x.png`)
+        mainImg.attr('src', `https://openweathermap.org/img/wn/${data2.list[0].weather[0].icon}@4x.png`)
         mainCondition.text(data2.list[0].weather[0].main.toUpperCase());
         wind.text(`${data2.list[0].wind.speed} m/s`);
         pressure.text(`${data2.list[0].main.pressure} hPa`);
